f75c9ddc250c44476ffa406e2c8d2136d1a7c5bc
mercuricchloride <mercuryprotocol@protonmail.com>
2023-05-08 19:16:48 -0700

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,42 @@
 mod pb;
+mod tables;
 
-use pb::sf::ethereum::block_meta::v1::BlockMeta;
+use pb::test::{Contract, Contracts};
 
 use substreams::Hex;
+use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_ethereum::pb::eth;
 
-#[substreams::handlers::map]
-fn map_block(block: eth::v2::Block) -> Result<BlockMeta, substreams::errors::Error> {
-    let header = block.header.as_ref().unwrap();
+use tables::Tables;
 
-    Ok(BlockMeta {
-        number: block.number,
-        hash: Hex(&block.hash).to_string(),
-        parent_hash: Hex(&header.parent_hash).to_string(),
-        timestamp: header.timestamp.as_ref().unwrap().to_string(),
-    })
+#[substreams::handlers::map]
+fn map_contract(block: eth::v2::Block) -> Result<Contracts, substreams::errors::Error> {
+    let contracts = block
+        .transactions()
+        .flat_map(|tx| {
+            tx.calls
+                .iter()
+                .filter(|call| call.call_type == 5)
+                .map(|call| Contract {
+                    address: format!("0x{}", Hex(&call.address).to_string()),
+                    timestamp: block.timestamp_seconds().to_string(),
+                    ordinal: tx.begin_ordinal,
+                })
+        })
+        .collect();
+    Ok(Contracts { contracts })
+}
+
+#[substreams::handlers::map]
+pub fn graph_out(contracts: Contracts) -> Result<EntityChanges, substreams::errors::Error> {
+    // hash map of name to a table
+    let mut tables = Tables::new();
+
+    for contract in &contracts.contracts {
+        tables
+            .create_row("Contract", &contract.address)
+            .set("timestamp", &contract.timestamp);
+    }
+
+    Ok(tables.to_entity_changes())
 }
--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,4 +1,11 @@
 // @generated
+pub mod google {
+    // @@protoc_insertion_point(attribute:google.protobuf)
+    pub mod protobuf {
+        include!("google.protobuf.rs");
+        // @@protoc_insertion_point(google.protobuf)
+    }
+}
 pub mod sf {
     pub mod ethereum {
         pub mod block_meta {
@@ -9,4 +16,16 @@ pub mod sf {
             }
         }
     }
+    pub mod substreams {
+        // @@protoc_insertion_point(attribute:sf.substreams.v1)
+        pub mod v1 {
+            include!("sf.substreams.v1.rs");
+            // @@protoc_insertion_point(sf.substreams.v1)
+        }
+    }
+}
+// @@protoc_insertion_point(attribute:test)
+pub mod test {
+    include!("test.rs");
+    // @@protoc_insertion_point(test)
 }
